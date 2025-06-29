3302fa9d6312a6d6a9de7419f8d81c2331d0e55b
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-06-29 14:15:03 -0700

    Style: Implement basic home page UI including total balance and charts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
+export const dynamic = 'force-dynamic';
+
 import type { Metadata } from 'next';
 import { IBM_Plex_Serif, Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({
-	variable: '--font-inter',
-	subsets: ['latin'],
-});
-
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
 	subsets: ['latin'],
 	weight: ['400', '700'],
@@ -17,7 +15,7 @@ export const metadata: Metadata = {
 	title: 'Horizon',
 	description: 'Horizon is a modern banking platform for everyone.',
 	icons: {
-		icon: './icons/logo.svg',
+		icon: '/icons/logo.svg',
 	},
 };
 
