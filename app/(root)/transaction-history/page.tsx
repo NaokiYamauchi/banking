56fa14f702a61eb56862500adf09d352ea0bb57e
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-07-05 16:24:00 -0700

    Create a page that shows detailed transactions of each account

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,6 +1,8 @@
 import HeaderBox from '@/components/HeaderBox';
+import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { formatAmount } from '@/lib/utils';
 
 const TransactionHistory = async ({
 	searchParams: { id, page },
@@ -28,9 +30,28 @@ const TransactionHistory = async ({
 			<div className="space-y-6">
 				<div className="transactions-account">
 					<div className="flex flex-col gap-2">
-						{/* TODO: <h2>5:28:41</h2> */}
+						<h2 className="text-18 font-bold text-white">
+							{account?.data.name}
+						</h2>
+						<p className="text-14 text-blue-25">
+							{account?.data.officialName}
+						</p>
+						<p className="text-14 font-semibold tracking-[1.1px] text-white">
+							●●●● ●●●● ●●●● {account?.data.mask}
+						</p>
+					</div>
+
+					<div className="transactions-account-balance">
+						<p className="text-14">Current balance</p>
+						<p className="text-24 text-center font-bold">
+							{formatAmount(account?.data.currentBalance)}
+						</p>
 					</div>
 				</div>
+
+				<section className="flex w-full flex-col gap-6">
+					<TransactionsTable transactions={account?.transactions} />
+				</section>
 			</div>
 		</div>
 	);
